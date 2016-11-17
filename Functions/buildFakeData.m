2fb2442e271ba7111c4187fee7e1091b0f40cc9e
Matthew Kemnetz <mkemnetz@nd.edu>
2016-11-17 12:42:00 -0500

    Adding buildfakedata
    Added the buildfakedata function which takes in the large 5in
    wavefronts and builds four sets of data.  These represent separations
    of 0, 1, 2, and 3 boundary layer thicknesses.

---
+++ Functions/buildFakeData.m
@@ -0,0 +1,51 @@
+function [data] = buildfakedata(WF) 
+%BUILDFAKEDATA - This function takes in the large 5in wavefronts and builds
+%four sets of data.  These represent separations of 0, 1, 2, and 3 boundary
+%layer thicknesses.
+% 
+% Syntax:  [data] = buildfakedata(WF) 
+% 
+% Inputs: 
+%    WF - A 3D array or a cell array carrying OPD.  The third dimension is
+%           time.
+%
+% Outputs: 
+%    data - A struct holding the data of the original data and the three
+%           separations.
+%           data.zero  = zero separation or the "upstream" data
+%           data.one   = one delta separation
+%           data.two   = two delta separation
+%           data.three = three delta separation
+%           data.four  = four delta separation
+% 
+% Example: 
+%    [data] = buildfakedata(WF)  
+% 
+% Other m-files required: none 
+% Subfunctions: none 
+% MAT-files required: none 
+% 
+% See also: OTHER_FUNCTION_NAME1,  OTHER_FUNCTION_NAME2 
+ 
+% Author: Matthew Kemnetz 
+% Hessert Laboratory for Aerospace Research B034 
+% email: mkemnetz@nd.edu, kemnetz.m@gmail.com 
+% Website: http://www.matthewkemnetz.com 
+% November 2016; Last revision: 17-November-2016 
+% Copyright 2016, Matthew Kemnetz, All rights reserved. 
+ 
+%% ------------- BEGIN CODE -------------- %% 
+ 
+Enter your executable matlab commands here 
+
+
+
+
+
+
+
+
+
+
+ % ===== EOF ====== [buildfakedata.m] ======  
+
